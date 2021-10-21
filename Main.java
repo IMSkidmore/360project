c937034404d6ec9afe7641fdc762f8e25f228181
AussieAlayer <89155493+AussieAlayer@users.noreply.github.com>
2021-10-21 16:19:35 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-//general login screen
-
 package application;
 
 import javafx.application.Application;
@@ -27,12 +25,41 @@ public class Main extends Application {
 	private Stage stage;
 	private TextField username = new TextField();
 	private PasswordField password = new PasswordField();
-	private Scene scene1;
+	private Scene scene0, scene1, scene2;
+	private int choice;
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	public void start(Stage primaryStage) {
+		
+		//Layout for which login screen
+		VBox selectTypeLayout = new VBox();
+		StackPane stackLayout0 = new StackPane();
+		selectTypeLayout.setSpacing(8);
+		selectTypeLayout.setPadding(new Insets(10,10,10,10));
+		HBox hBoxLayout = new HBox();
+		hBoxLayout.setSpacing(8);
+		hBoxLayout.setPadding(new Insets(10,10,10,10));
+		Button doctorLogin = new Button("Doctor");
+		Button nurseLogin = new Button("Nurse");
+		Button patientLogin = new Button("Patient");
+		hBoxLayout.getChildren().addAll(doctorLogin, nurseLogin, patientLogin);
+		selectTypeLayout.getChildren().addAll(new Label("Please select an account type:"), hBoxLayout);
+		stackLayout0.getChildren().addAll(selectTypeLayout);
+		scene0 = new Scene(stackLayout0, 400,600);
+		
+		//Button action handler events
+		doctorLogin.setOnAction(e -> {choice = 0; 
+		primaryStage.setScene(scene1);
+		});
+		nurseLogin.setOnAction(e -> {choice = 1; 
+		primaryStage.setScene(scene1);
+		});
+		patientLogin.setOnAction(e -> {choice = 2; 
+		primaryStage.setScene(scene1);
+		});
+		
 		
 		//Layout for login screen
 		VBox vBoxLayout1 = new VBox();
@@ -41,11 +68,11 @@ public class Main extends Application {
 		Button login = new Button("Login");
 		vBoxLayout1.getChildren().addAll(new Label("Username"), username, new Label("Password"), password, login);
 		root.getChildren().addAll(vBoxLayout1);
-		Scene scene = new Scene(root, 400, 600);
+		scene1 = new Scene(root, 400, 600);
 		
 		//Event for button login
 		login.setOnAction(e -> {if(username.getText().equals("Austin") && password.getText().equals("Piper")) {
-			primaryStage.setScene(scene1);
+			primaryStage.setScene(scene2);
 		}else if(username.getText().equals("Austin") == false){
 			AlertBox.display("Login Error", "Username is incorrect, check capslock");
 		}else {
@@ -55,14 +82,14 @@ public class Main extends Application {
 		
 		//layout for scene1
 		Button logout = new Button("Logout");
-		logout.setOnAction(e -> primaryStage.setScene(scene));
+		logout.setOnAction(e -> primaryStage.setScene(scene0));
 		StackPane stackLayout2= new StackPane() ;
 		stackLayout2.getChildren().add(logout);
-		scene1 = new Scene(stackLayout2, 600, 300);
+		scene2 = new Scene(stackLayout2, 600, 300);
 		
-		primaryStage.setScene(scene);
+		primaryStage.setScene(scene0);
 		primaryStage.show();
-		primaryStage.setTitle("Login Example JavaFX");
+		primaryStage.setTitle("Login");
 		primaryStage.setAlwaysOnTop(true);
 	}
 }
