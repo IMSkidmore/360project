7814735269c2bf0b12e8b19aa9acfb459e061750
AussieAlayer <89155493+AussieAlayer@users.noreply.github.com>
2021-11-04 17:55:16 -0700

    Add files via upload

---
+++ Pharmacy.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pharmacy {
+	private String comp, add, num;
+	private List<String> list;
+	public Pharmacy(String name, String address, String phoneNumber) {
+		comp = name;
+		add = address;
+		num = phoneNumber;
+		list = new ArrayList<String>();
+	}
+	public void setPrescription(String pres) {
+		list.add(pres);
+	}
+}
