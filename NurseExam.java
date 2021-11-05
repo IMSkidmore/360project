1831df1ef2be7ae2a2ba850e3d58cc1d5756cd06
Ellis Osborn <84072644+ellisosborn00@users.noreply.github.com>
2021-11-04 18:24:49 -0700

    Update NurseExam.java
    Changed VBoxes to HBoxes

--- NurseExam.java
+++ NurseExam.java
@@ -1,7 +1,8 @@
 //patient vitals screen for nurse/doctor
 
-package vitalsinformation;
+package com.example.demo1;
 
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.paint.Color;
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -17,72 +18,87 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class vitalsinfo extends Application {
+public class nurseExam extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
 
         //Vertical and horizontal boxes for components
-        VBox root = new VBox();
-        VBox root2 = new VBox();
-        HBox root4 = new HBox();
+        HBox HRandWeightLabelBox = new HBox();
+        HBox HRandWeightTFBox = new HBox();
+        HBox  tempandBloodPressureLabelBox= new HBox();
+        HBox  tempandBloodPressureTFBox= new HBox();
+        HBox  HeightLabelBox= new HBox();
+        HBox  HeightTFBox= new HBox();
 
 
         //Initializing the labels
         Label heartLabel = new Label("Heart Rate");
-        Label weightLabel = new Label("Weight");
-        Label tempLabel = new Label("Temperature");
-        Label pressureLabel = new Label("Blood Pressure");
-        Label heightLabel = new Label("Height");
+        TextField heartTF = new TextField();
+        heartTF.setPromptText("BPM");
+        heartTF.setMaxWidth(150);
 
-        //Initializing the textfields and setting their prompt text and sizes
-        TextField tf1 = new TextField();
-        tf1.setPromptText("BPM");
-        tf1.setMaxWidth(150);
-        TextField tf2 = new TextField();
-        tf2.setPromptText("Lbs");
-        tf2.setMaxWidth(150);
-        TextField tf3 = new TextField();
-        tf3.setPromptText("F");
-        tf3.setMaxWidth(150);
-        TextField tf4 = new TextField();
-        tf4.setPromptText("mmHg");
-        tf4.setMaxWidth(150);
-        TextField tf5 = new TextField();
-        tf5.setPromptText("FT");
-        tf5.setMaxWidth(100);
-        TextField tf6 = new TextField();
-        tf6.setPromptText("Inches");
-        tf6.setMaxWidth(100);
+        Label weightLabel = new Label("                                                                                         Weight");
+        TextField weigthTF = new TextField();
+        weigthTF.setPromptText("Lbs");
+        weigthTF.setMaxWidth(150);
+        Label space1 = new Label("                                                             ");
+
+        Label tempLabel = new Label("Temperature                                                                                      ");
+        TextField tempTF = new TextField();
+        tempTF.setPromptText("F");
+        tempTF.setMaxWidth(150);
+
+        Label pressureLabel = new Label("Blood Pressure");
+        TextField pressureTF= new TextField();
+        pressureTF.setPromptText("mmHg");
+        pressureTF.setMaxWidth(150);
+        Label space2 = new Label("                                                             ");
+
+        Label heightLabel = new Label("Height                                                                                               ");
+        TextField heightTF = new TextField();
+        heightTF.setPromptText("FT");
+        heightTF.setMaxWidth(75);
+        TextField inchTF = new TextField();
+        inchTF.setPromptText("Inches");
+        inchTF.setMaxWidth(75);
+
+        Label allergyLabel = new Label("Allergy");
+        TextField allergyTF = new TextField();
+        allergyTF.setPromptText("Latex,Penicillin,etc.");
+        allergyTF.setMaxWidth(150);
+        Label space3 = new Label("                                                            ");
 
 
         //Creation of the "Submit" button
         Button b = new Button("Submit");
 
 
-
         //Adding the components to their respective container, and setting the spaces
-        root.getChildren().addAll(heartLabel,tf1,tempLabel,tf3,heightLabel,tf5,tf6);
-        root.setSpacing(10);
-        root2.getChildren().addAll(weightLabel,tf2,pressureLabel,tf4,b);
-        root2.setSpacing(10);
-        root4.getChildren().addAll(tf5,tf6);
-        root4.setSpacing(5);
-
-
+        HRandWeightLabelBox.getChildren().addAll(heartLabel,weightLabel);
+        HRandWeightTFBox.getChildren().addAll(heartTF,space1,weigthTF);
+        tempandBloodPressureLabelBox.getChildren().addAll(tempLabel,pressureLabel);
+        tempandBloodPressureTFBox.getChildren().addAll(tempTF,space2,pressureTF);
+        HeightLabelBox.getChildren().addAll(heightLabel,allergyLabel);
+        HeightTFBox.getChildren().addAll(heightTF,inchTF,space3,allergyTF);
 
 
         //This is the container that will contain all the sub-containers created above
-        GridPane root3 = new GridPane();
-        root3.setHgap(150);
-        root3.addColumn(0, root);
-        root3.addColumn(1, root2);
-        root3.addRow(1, root4);
-        root3.setStyle("-fx-background-color: #a2f3f5;");
+        GridPane pane = new GridPane();
+        pane.setVgap(3);
+
+        pane.add(HRandWeightLabelBox,0,0);
+        pane.add(HRandWeightTFBox,0,1);
+        pane.add(tempandBloodPressureLabelBox,0,2);
+        pane.add(tempandBloodPressureTFBox,0,3);
+        pane.add(HeightLabelBox,0,4);
+        pane.add(HeightTFBox,0,5);
+
+        pane.setStyle("-fx-background-color: #a2f3f5;");
 
         //Adding the major container created above to the scene and setting the scene up
-        Scene scene = new Scene(root3, 550, 300);
+        Scene scene = new Scene(pane, 550, 300);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Input Patient Vital Information");
         primaryStage.show();
