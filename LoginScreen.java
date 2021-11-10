43072a330311910cdd820a2c154b6632c30ba5e6
AussieAlayer <89155493+AussieAlayer@users.noreply.github.com>
2021-11-10 12:48:20 -0700

    Update LoginScreen.java

--- LoginScreen.java
+++ LoginScreen.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -24,9 +29,11 @@ public class LoginScreen extends StackPane {
 	private TextField username = new TextField();
 	private PasswordField password = new PasswordField();
 	public int choice;
+	private Stage stageLogin;
 	public LoginScreen(Stage primaryStage, int width, int height) {		
 		
 		// Layout for login screen
+		stageLogin = primaryStage;
 		VBox vBoxLayout1 = new VBox();
 		vBoxLayout1.setSpacing(8);
 		vBoxLayout1.setPadding(new Insets(10, 10, 10, 10));
@@ -42,25 +49,33 @@ public class LoginScreen extends StackPane {
 	}
 	private class ButtonHandler implements EventHandler<ActionEvent> {
 		public void handle(ActionEvent event) {
-		try {
-			Scanner scanner = new Scanner(new FileReader("src/application/LoginEmployees.txt"));
-			String line;
+			try {
+				Scanner scanner = new Scanner(new FileReader("src/application/LoginEmployees.txt"));
+				String line;
 
 	            while (scanner.hasNext()) {
 	            	line = scanner.nextLine();
 	            	String[] tokens=line.split(",");
 	      
 	            	if (username.getText().equals(tokens[0]) && password.getText().equals(tokens[1])) {
-	    				//add change screen here
-	    			} else if (username.getText().equals(tokens[0])) {
+	            		if(tokens[2].equals("0")) {
+	            			DoctorDashboard doctorDash = new DoctorDashboard();
+	            			doctorDash.start(stageLogin);
+	            		}else {
+	            			NurseDashboard nurseDash = new NurseDashboard();
+	            			nurseDash.start(stageLogin);
+	            		}
+	            	} else if (username.getText().equals(tokens[0])) {
 	    				AlertBox.display("Login Error", "Password is incorrect, check capslock");
+	    			} else if(password.getText().equals(tokens[1])){
+	    				AlertBox.display("Login Error", "Username is incorrect, check capslock");
 	    			}
 	            }
 	            scanner.close();
 	        } catch (IOException e) {
 	            AlertBox.display("File not found", "Check file location");
 	        }
-			AlertBox.display("Login Error", "Username is incorrect, check capslock");
+			
 		}
 	} // end of ButtonHandler
 
