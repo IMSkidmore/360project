e981f8c01ab15e5b1bf3e450d21bb93dff824e9e
AussieAlayer <89155493+AussieAlayer@users.noreply.github.com>
2021-11-11 16:17:39 -0700

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -29,9 +29,9 @@ public class Parser {
 		    }
 	}
 	public static Patient lineToPatient(String str) {
-		String[] array = new String[6];
-        array=str.split(",");
-        return new Patient(array[0], array[1], array[2], array[3], array[4], array[5]);
+		String[] array = new String[12];
+        	array=str.split(",");
+        	return new Patient(array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7], array[8], array[9], array[10], array[11]);
 	}
 	public static String[] lineToHistoryArray(String str) {
 		String[] array = new String[12];
