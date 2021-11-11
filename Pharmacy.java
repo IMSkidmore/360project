93a92bc956d73279be57ffe92b2aaf9deb263df0
AussieAlayer <89155493+AussieAlayer@users.noreply.github.com>
2021-11-11 15:09:02 -0700

    Update Pharmacy.java

--- Pharmacy.java
+++ Pharmacy.java
@@ -6,6 +6,11 @@ import java.util.List;
 public class Pharmacy {
 	private String comp, add, num;
 	private List<String> list;
+	public Pharmacy() {
+		comp = "";
+		add = "";
+		num = "";
+	}
 	public Pharmacy(String name, String address, String phoneNumber) {
 		comp = name;
 		add = address;
