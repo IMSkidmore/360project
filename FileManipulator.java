42604797ce24ce184166a3ab51272c9cad583865
AussieAlayer <89155493+AussieAlayer@users.noreply.github.com>
2021-11-11 12:34:55 -0700

    Update FileManipulator.java

--- FileManipulator.java
+++ FileManipulator.java
@@ -11,7 +11,7 @@ public class FileManipulator {
 	public void writeFileContents(String fileName, ArrayList<String> array) throws IOException {
 		FileWriter filewrite;
 		try {
-			filewrite = new FileWriter(fileName);
+			filewrite = new FileWriter(fileName, true);
 
 			// Initialing BufferedWriter
 			BufferedWriter bufferwrite = new BufferedWriter(filewrite);
@@ -22,7 +22,6 @@ public class FileManipulator {
 			}
 
 			bufferwrite.close();
-			System.out.println("Written successfully");
 		} catch (IOException excpt) {
 			excpt.printStackTrace();
 		}
@@ -39,4 +38,28 @@ public class FileManipulator {
 			 fileContents.add(st);
 		return fileContents;
 	}
+	
+	public ArrayList<Patient> readFilePatientInfo() throws IOException{
+		 ArrayList<Patient> fromFile = new ArrayList<Patient>();
+		 File fi = new File("src/application/PatientInfo.txt"); 
+		 
+		 BufferedReader br = new BufferedReader(new FileReader(fi)); 
+		 		 String st;
+		 while ((st = br.readLine()) != null) 
+			 fromFile.add(Parser.lineToPatient(st));
+		return fromFile;
+	}
+	
+	public ArrayList<String[]> readFilePatientHistory(String id) throws IOException{
+		 File fi = new File("src/application/" + id + ".txt"); 
+		 
+		 BufferedReader br = new BufferedReader(new FileReader(fi)); 
+		 
+		 ArrayList<String[]> fileContents = new ArrayList<String[]>();
+		 String st;
+		 while ((st = br.readLine()) != null) 
+			 fileContents.add(Parser.lineToHistoryArray(st));
+		return fileContents;
+	}
+	
 }
