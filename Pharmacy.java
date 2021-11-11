8bdbf940088fb84475bfac3ac9c4a5d3a0d214c4
AussieAlayer <89155493+AussieAlayer@users.noreply.github.com>
2021-11-11 15:10:27 -0700

    Update Pharmacy.java

--- Pharmacy.java
+++ Pharmacy.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Pharmacy {
 	private String comp, add, num;
 	private List<String> list;
+	
 	public Pharmacy() {
 		comp = "";
 		add = "";
 		num = "";
+		list = new ArrayList<String>();
 	}
 	public Pharmacy(String name, String address, String phoneNumber) {
 		comp = name;
@@ -19,5 +21,23 @@ public class Pharmacy {
 	}
 	public void setPrescription(String pres) {
 		list.add(pres);
+	}
+	public String getComp() {
+		return comp;
+	}
+	public void setComp(String comp) {
+		this.comp = comp;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
 	}
 }
