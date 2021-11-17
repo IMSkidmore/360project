a733644c71b749608f8d0503c5d5f54049dddc68
AussieAlayer <89155493+AussieAlayer@users.noreply.github.com>
2021-11-17 15:06:23 -0700

    Update FileManipulator.java

--- FileManipulator.java
+++ FileManipulator.java
@@ -9,15 +9,14 @@ public class FileManipulator {
 	}
 
 	public void writeFileContents(String fileName, ArrayList<String> array) throws IOException {
-		File file = new File("src/application/"+fileName + ".txt");
-		if(file.exists() == false) {
+		File file = new File("src/application/" + fileName + ".txt");
+		if (file.exists() == false) {
 			file.createNewFile();
 		}
 		FileWriter filewrite;
 		try {
-			filewrite = new FileWriter("src/application/"+fileName+".txt", true);
-			
-			
+			filewrite = new FileWriter("src/application/" + fileName + ".txt", true);
+
 			// Initialing BufferedWriter
 			BufferedWriter bufferwrite = new BufferedWriter(filewrite);
 			for (String s : array) {
@@ -31,43 +30,44 @@ public class FileManipulator {
 		}
 	}
 
-	public ArrayList<String> readFileContents(String fileName) throws IOException{
-		 File fi = new File("src/application"+fileName+".txt"); 
-		 
-		 BufferedReader br = new BufferedReader(new FileReader(fi)); 
-		 
-		 ArrayList<String> fileContents = new ArrayList<String>();
-		 String st;
-		 while ((st = br.readLine()) != null) 
-			 fileContents.add(st);
+	public ArrayList<String> readFileContents(String fileName) throws IOException {
+		File fi = new File("src/application" + fileName + ".txt");
+
+		BufferedReader br = new BufferedReader(new FileReader(fi));
+
+		ArrayList<String> fileContents = new ArrayList<String>();
+		String st;
+		while ((st = br.readLine()) != null)
+			fileContents.add(st);
 		return fileContents;
 	}
-	
-	public ArrayList<Patient> readFilePatientInfo() throws IOException{
-		 ArrayList<Patient> fromFile = new ArrayList<Patient>();
-		 File fi = new File("src/application/PatientInfo.txt"); 
-		 
-		 BufferedReader br = new BufferedReader(new FileReader(fi)); 
-		 		 String st;
-		 while ((st = br.readLine()) != null) 
-			 fromFile.add(Parser.lineToPatient(st));
+
+	public ArrayList<String[]> readFilePatientHistory(String id) throws IOException {
+
+		File fi = new File("src/application/" + id + ".txt");
+		BufferedReader br = new BufferedReader(new FileReader(fi));
+
+		ArrayList<String[]> fileContents = new ArrayList<String[]>();
+		String st;
+		while ((st = br.readLine()) != null)
+			fileContents.add(Parser.lineToHistoryArray(st));
+		return fileContents;
+	}
+
+	public ArrayList<Patient> readFilePatientInfo() throws IOException {
+		ArrayList<Patient> fromFile = new ArrayList<Patient>();
+		File fi = new File("src/application/PatientInfo.txt");
+
+		BufferedReader br = new BufferedReader(new FileReader(fi));
+		String st;
+		while ((st = br.readLine()) != null)
+			fromFile.add(Parser.lineToPatient(st));
 		return fromFile;
 	}
-	
-	public ArrayList<String[]> readFilePatientHistory(String id) throws IOException{
-		
-		 File fi = new File("src/application/" + id + ".txt"); 
-		 BufferedReader br = new BufferedReader(new FileReader(fi)); 
-		 
-		 ArrayList<String[]> fileContents = new ArrayList<String[]>();
-		 String st;
-		 while ((st = br.readLine()) != null) 
-			 fileContents.add(Parser.lineToHistoryArray(st));
-		return fileContents;
-	}
+
 	public void writeFileContentsPatientInfo(ArrayList<String> array) throws IOException {
 		File file = new File("src/application/PatientInfo.txt");
-		if(file.exists() == false) {
+		if (file.exists() == false) {
 			file.createNewFile();
 		}
 		FileWriter filewrite;
@@ -75,13 +75,12 @@ public class FileManipulator {
 			filewrite = new FileWriter("src/application/PatientInfo.txt", true);
 			// Initialing BufferedWriter
 			BufferedWriter bufferwrite = new BufferedWriter(filewrite);
-			
+
 			int i = 0;
 			for (String s : array) {
-				if(i >= 11) {
+				if (i >= 11) {
 					bufferwrite.write(s);
-				}
-				else if(i < 11) {
+				} else if (i < 11) {
 					bufferwrite.write(s + ",");
 				}
 				i++;
@@ -93,5 +92,79 @@ public class FileManipulator {
 			excpt.printStackTrace();
 		}
 	}
-	
+	public String[] readFileContentsVitals(String fileName) throws IOException {
+		File fi = new File("src/application/" + fileName + "Vitals.txt");
+
+		BufferedReader input = new BufferedReader(new FileReader(fi));
+	    String last=""; 
+	    String line;
+
+	    while ((line = input.readLine()) != null) { 
+	        last = line;
+	    }
+		String[] fromFile = last.split(",");
+		fi.delete();
+		return fromFile;
+	}
+
+	public void writeFileContentsVitals(String fileName, ArrayList<String> array) throws IOException {
+		File file = new File("src/application/" + fileName + "Vitals.txt");
+		if (file.exists() == false) {
+			file.createNewFile();
+		}
+		FileWriter filewrite;
+		try {
+			filewrite = new FileWriter("src/application/" + fileName + ".txt", true);
+			// Initialing BufferedWriter
+			BufferedWriter bufferwrite = new BufferedWriter(filewrite);
+
+			int i = 0;
+			for (String s : array) {
+				if (i >= 5) {
+					bufferwrite.write(s);
+				} else if (i < 5) {
+					bufferwrite.write(s + ",");
+				}
+				i++;
+			}
+			bufferwrite.newLine();
+
+			bufferwrite.close();
+		} catch (IOException excpt) {
+			excpt.printStackTrace();
+		}
+	}
+
+	public void writeFileContentsExam(String fileName, ArrayList<String> array) throws IOException {
+		String[] vitals = readFileContentsVitals(fileName);
+		for(String s: vitals) {
+			array.add(s);
+		}
+		File file = new File("src/application/" + fileName + ".txt");
+		if (file.exists() == false) {
+			file.createNewFile();
+		}
+		FileWriter filewrite;
+		try {
+			filewrite = new FileWriter("src/application/" + fileName + ".txt", true);
+			// Initialing BufferedWriter
+			BufferedWriter bufferwrite = new BufferedWriter(filewrite);
+
+			int i = 0;
+			for (String s : array) {
+				if (i >= 11) {
+					bufferwrite.write(s);
+				} else if (i < 11) {
+					bufferwrite.write(s + ",");
+				}
+				i++;
+			}
+			bufferwrite.newLine();
+
+			bufferwrite.close();
+		} catch (IOException excpt) {
+			excpt.printStackTrace();
+		}
+	}
+
 }
