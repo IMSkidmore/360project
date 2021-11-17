9c6974d877501c156cf1c88272b2f7798548d90d
ColorlessSmell <62539812+ColorlessSmell@users.noreply.github.com>
2021-11-17 13:23:46 -0700

    Update LoginScreen.java

--- LoginScreen.java
+++ LoginScreen.java
@@ -28,57 +28,79 @@ import javafx.scene.layout.VBox;
 import javafx.scene.control.PasswordField;
 
 public class LoginScreen extends StackPane {
-	private TextField username = new TextField();
-	private PasswordField password = new PasswordField();
-	public int choice;
-	private Stage stageLogin;
-	public LoginScreen(Stage primaryStage, int width, int height) {		
-		
-		// Layout for login screen
-		stageLogin = primaryStage;
-		VBox vBoxLayout1 = new VBox();
-		vBoxLayout1.setSpacing(8);
-		vBoxLayout1.setPadding(new Insets(10, 10, 10, 10));
-		Button login = new Button("Login");
-		Button newUser = new Button("New User");
-		vBoxLayout1.getChildren().addAll(new Label("Username"), username, new Label("Password"), password, login);
-		this.getChildren().addAll(vBoxLayout1);
+    private TextField username = new TextField();
+    private PasswordField password = new PasswordField();
+    public int choice;
+    private Stage stageLogin;
+    private Stage mainStage;
 
-		// Event for button login
-		EventHandler loginHandle = new ButtonHandler();
-		login.setOnAction(loginHandle);
+    public LoginScreen(Stage primaryStage, int width, int height) {
 
-	}
-	private class ButtonHandler implements EventHandler<ActionEvent> {
-		public void handle(ActionEvent event) {
-			try {
-				Scanner scanner = new Scanner(new FileReader("src/application/LoginEmployees.txt"));
-				String line;
+        // Layout for login screen
+        mainStage = primaryStage;
+        stageLogin = primaryStage;
+        VBox vBoxLayout1 = new VBox();
+        vBoxLayout1.setSpacing(8);
+        vBoxLayout1.setPadding(new Insets(10, 10, 10, 10));
+        Button login = new Button("Login");
+        Button newUser = new Button("New User");
+        Button cancel = new Button("Cancel");
+        vBoxLayout1.getChildren().addAll(new Label("Username"), username, new Label("Password"), password, login, cancel);
+        this.getChildren().addAll(vBoxLayout1);
 
-	            while (scanner.hasNext()) {
-	            	line = scanner.nextLine();
-	            	String[] tokens=line.split(",");
-	      
-	            	if (username.getText().equals(tokens[0]) && password.getText().equals(tokens[1])) {
-	            		if(tokens[2].equals("0")) {
-	            			DoctorDashboard doctorDash = new DoctorDashboard();
-	            			doctorDash.start(stageLogin);
-	            		}else {
-	            			NurseDashboard nurseDash = new NurseDashboard();
-	            			nurseDash.start(stageLogin);
-	            		}
-	            	} else if (username.getText().equals(tokens[0])) {
-	    				AlertBox.display("Login Error", "Password is incorrect, check capslock");
-	    		} else if (!username.getText().equals(tokens[0]) && !password.getText().equals(tokens[1])){
-	    				AlertBox.display("Login Error", "No such account found");
-	    		}
-	            }
-	            scanner.close();
-	        } catch (IOException e) {
-	            AlertBox.display("File not found", "Check file location");
-	        }
-			
-		}
-	} // end of ButtonHandler
+        // Event for button login
+        EventHandler loginHandle = new ButtonHandler();
+        login.setOnAction(loginHandle);
+
+        EventHandler cancelled = new cancelHandler();
+        cancel.setOnAction(cancelled);
+
+    }
+    private class ButtonHandler implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent event) {
+            try {
+                Scanner scanner = new Scanner(new FileReader("src/application/LoginEmployees.txt"));
+                String line;
+
+                while (scanner.hasNext()) {
+                    line = scanner.nextLine();
+                    String[] tokens=line.split(",");
+
+                    if (username.getText().equals(tokens[0]) && password.getText().equals(tokens[1])) {
+                        if(tokens[2].equals("0")) {
+                            DoctorDashboard doctorDash = new DoctorDashboard();
+                            doctorDash.start(stageLogin);
+                        }else {
+                            NurseDashboard nurseDash = new NurseDashboard();
+                            nurseDash.start(stageLogin);
+                        }
+                    } else if (username.getText().equals(tokens[0])) {
+                        AlertBox.display("Login Error", "Password is incorrect, check capslock");
+                    } else if (!username.getText().equals(tokens[0]) && !password.getText().equals(tokens[1])){
+                        AlertBox.display("Login Error", "No such account found");
+                    }
+                }
+                scanner.close();
+            } catch (IOException e) {
+                AlertBox.display("File not found", "Check file location");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    } // end of ButtonHandler
+
+    private class cancelHandler implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent event) {
+
+            Main cancelDash = new Main();
+
+            try {
+                cancelDash.start(mainStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
