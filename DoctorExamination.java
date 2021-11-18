d6c5865bba31f58f2e46b257db863f7e557b365e
ColorlessSmell <62539812+ColorlessSmell@users.noreply.github.com>
2021-11-17 20:28:08 -0700

    Update DoctorExamination.java

--- DoctorExamination.java
+++ DoctorExamination.java
@@ -1,25 +1,27 @@
-//The screen for the actual exam for the doctor. Provides doctor with all necessary options to save information
-
-//Declares all necessary imports
-package application;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class DoctorExamination extends Application {
-    @Override
+
+    private Stage saved;
+    private Stage cancelled;
+    private EventHandler savee = new saveHandler();
+    private EventHandler cancell = new cancelHandler();
+
     public void start(Stage stage) throws Exception {
-        
-        //builds label  and sets font size, then creates a new label for reflexes and sets that font size as well.
-        //Then builds all the radio buttons for the doctor to put in the reflexes of the patient.
-        //Also sets up an area for the doctor to input other physical tests.
+
+        saved = stage;
+        cancelled = stage;
+
         VBox vBox = new VBox(15);
+        vBox.setStyle("-fx-background-color: #a2f3f5;");
         Label top = new Label("Examination for (Insert Name here)");
         top.setAlignment(Pos.CENTER);
         top.setStyle("-fx-font-size: 18px");
@@ -33,19 +35,74 @@ public class DoctorExamination extends Application {
         HBox hBox = new HBox(20);
         hBox.getChildren().addAll(reflex, radioButton0, radioButton1, radioButton2,
                 radioButton3, radioButton4);
-        Label other = new Label("(Other physcial tests)");
-        other.setStyle("-fx-font-size: 16px");
+        Label eyes = new Label("Eye Test:");
+        eyes.setStyle("-fx-font-size: 13px");
+        TextField eye = new TextField();
+        eye.setPromptText("Eye examination results");
+        eye.setMaxWidth(250);
+        Label ear = new Label("Ear Test:");
+        ear.setStyle("-fx-font-size: 13px");
+        TextField ears = new TextField();
+        ears.setPromptText("Ear examination results");
+        ears.setMaxWidth(250);
+        TextField throat = new TextField();
+        throat.setPromptText("Throat examination results");
+        throat.setMaxWidth(250);
+        Label throats = new Label("Throat Test:");
+        throats.setStyle("-fx-font-size: 13px");
+        Label medication = new Label("Prescribe Medication");
+        medication.setStyle("-fx-font-size: 13px");
+        TextField medi = new TextField();
+        medi.setPromptText("Prescription");
+        medi.setMaxWidth(250);
+        Label summary = new Label("Health History");
+        summary.setStyle("-fx-font-size: 13px");
+        TextArea history = new TextArea();
+        history.setPromptText("Enter Health History");
         Button cancel = new Button("Cancel");
+        cancel.setOnAction(cancell);
         Button save = new Button("Save");
+        save.setOnAction(savee);
 
         HBox hBox1 = new HBox(30);
         hBox1.getChildren().addAll(cancel, save);
         hBox1.setAlignment(Pos.CENTER);
+        HBox summ = new HBox(30);
+        summ.getChildren().addAll(summary, history);
 
-        vBox.getChildren().addAll(top,hBox,other,hBox1);
-        stage.setScene(new Scene(vBox,600,400));
+        vBox.getChildren().addAll(top, hBox, summ, eyes, eye, ear, ears, throats, throat, medication, medi, hBox1);
+        stage.setScene(new Scene(vBox, 600, 400));
         stage.show();
+        stage.setTitle("Doctor Exam");
 
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private class saveHandler implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent event) {
+
+            DoctorDashboard saveDash = new DoctorDashboard();
+            try {
+                saveDash.start(saved);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class cancelHandler implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent event) {
+
+            DoctorDashboard cancelDash = new DoctorDashboard();
+            try {
+                cancelDash.start(cancelled);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
