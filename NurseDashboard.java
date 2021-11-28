569de084e070192fe25803076ce91abe4a0d9a66
ColorlessSmell <62539812+ColorlessSmell@users.noreply.github.com>
2021-11-28 15:57:57 -0700

    Update NurseDashboard.java

--- NurseDashboard.java
+++ NurseDashboard.java
@@ -28,15 +28,22 @@ public class NurseDashboard extends Application {
     private Label name;
     private Patient p;
     private ArrayList<String> indexList;
+    private Stage mainStage;
 
     public void start(Stage primaryStage) throws IOException {
-
+        
+        //Logout Button and Handler
+        Button logout = new Button("Logout");
+        EventHandler logoutt = new cancelHandler();
+        logout.setOnAction(logoutt);
+        
         //file manipulator object
         FileManipulator fm = new FileManipulator();
         array = fm.readFilePatientInfo();
         indexList = new ArrayList<String>();
 
         stageLogin = primaryStage;
+        mainstage = primaryStage;
         GridPane pane = new GridPane();
         pane.setStyle("-fx-background-color: #a2f3f5;");
         pane.setVgap(20);
@@ -84,6 +91,7 @@ public class NurseDashboard extends Application {
         pane.add(vbox2,1,1);
         pane.add(vBox1,2,1);
         pane.add(listView,0,2);
+        pane.add(logout,1,2);
 
         stageLogin.setScene(new Scene(pane,600,400));
         stageLogin.show();
@@ -142,4 +150,18 @@ public class NurseDashboard extends Application {
             }
         }
     }
+    
+    private class cancelHandler implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent event) {
+
+            Main cancelDash = new Main();
+
+            try {
+                cancelDash.start(mainStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
 }
